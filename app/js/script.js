cafffffe612c44f8b1dfc4156f806d3264609b05
Sayonil Mitra <sayonilmitra@gmail.com>
2022-06-27 11:01:41 +0530

    Added toggle menu for mobile devices

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+const btnHamburgur = document.getElementById("btnHamburgur");
+const overlay = document.getElementById("overlay");
+
+btnHamburgur.addEventListener("click", function() {
+    if (btnHamburgur.classList.contains("open")){
+        btnHamburgur.classList.remove("open");
+        overlay.style.display = "none";
+    }
+    else{
+        btnHamburgur.classList.add("open");
+        overlay.style.display = "block";
+    }
+})
